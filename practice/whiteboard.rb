0ca1215720c0531742bb4874efb5347f48e6fe7d
Jason McKenzie <jmac.atx@gmail.com>
2021-06-27 11:03:06 -0500

    adds more practice problems

--- practice/whiteboard.rb
+++ practice/whiteboard.rb
@@ -32,28 +32,34 @@
 # smallfunction
 
 # create a function
-def smallest
-  # initialize an array
-  input = ["word", "eight", "adfadsf", "a", "adasadfasfa", "wor", "b"]
-  # initialize an index
-  index = 0
-  # initialize a variable to keep track of the smallest as we iterate through the array
-  smallest = input[0]
+# def smallest
+#   # initialize an array
+#   input = ["word", "eight", "adfadsf", "a", "adasadfasfa", "wor", "b"]
+#   # initialize an index
+#   index = 0
+#   # initialize a variable to keep track of the smallest as we iterate through the array
+#   smallest = input[0]
 
-  # create a while loop to iterate through the array based on it's length
-  while index < input.length
-    # create an if conditional to look for the smaller of our variable or the current array item
-    if input[index].length < smallest.length
-      # set the smaller of them to the variable
-      smallest = input[index]
-    end
-    # increment our index
-    index = index + 1
-  end
+#   # create a while loop to iterate through the array based on it's length
+#   while index < input.length
+#     # create an if conditional to look for the smaller of our variable or the current array item
+#     if input[index].length < smallest.length
+#       # set the smaller of them to the variable
+#       smallest = input[index]
+#     end
+#     # increment our index
+#     index = index + 1
+#   end
 
-  # print the result
-  p smallest
+#   # print the result
+#   p smallest
+# end
+
+# # call the function
+# smallest
+
+def productof(number1, number2, number3)
+  number1 * number2 * number3
 end
 
-# call the function
-smallest
+puts productof(2, 3, 6)
