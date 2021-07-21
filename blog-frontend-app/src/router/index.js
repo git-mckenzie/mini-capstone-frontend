290903054fd360a1273b73b6edc3360fe285bde8
Jason McKenzie <jmac.atx@gmail.com>
2021-07-21 12:56:10 -0500

    creates app with test and signup pages

---
+++ blog-frontend-app/src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Test from "../views/Test.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", name: "Home", component: Home, },
+  { path: "/about", name: "About", component: () => import("../views/About.vue"), },
+  { path: "/test", name: "Test", component: Test, },
+  { path: "/signup", name: "Signup", component: Signup,},
+  { path: "/login", name: "login", component: Login },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
