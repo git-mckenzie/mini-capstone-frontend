14cc0cce0dab3fa5670b7cb10e1e80f68fab19fd
Jason McKenzie <jmac.atx@gmail.com>
2021-07-22 10:08:26 -0500

    adds vue movie index

---
+++ movie-vue-app/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+<style></style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      movies: [],
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        this.movies = response.data;
+        console.log("All movies:", this.movies);
+      });
+    },
+  },
+};
+</script>
