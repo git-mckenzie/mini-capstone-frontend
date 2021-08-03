f6596f47eb6f4f295c8df186ce73759cb8b4cc38
Jason McKenzie <jmac.atx@gmail.com>
2021-08-03 13:06:21 -0500

    creates vue-notes-app

---
+++ vue-notes/src/components/NoteSelector.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="note-selector active">
+    <p class="note-selector-title">First note...</p>
+    <p class="note-selector-timestamp">Timestamp here...</p>
+  </div>
+</template>
+
+<script>
+// import NoteSelectors from "./NoteSelectors.vue";
+// import NoteEditor from "./NoteEditor.vue";
+
+export default {
+  components: {
+    NoteSelector,
+  },
+};
+</script>
