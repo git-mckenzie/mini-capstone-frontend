f3379be18bf5e2bb3eaa9fd3aceccdc1fffc4923
Jason McKenzie <jmac.atx@gmail.com>
2021-08-03 14:15:25 -0500

    adds note selector

--- vue-notes/src/components/NoteSelectors.vue
+++ vue-notes/src/components/NoteSelectors.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="note-selectors">
-    <NoteSelector />
-    <NoteSelector />
-    <NoteSelector />
+    <NoteSelector
+      v-for="note in notes"
+      v-bind:note="note"
+      v-bind:selectedNoteId="selectedNoteId"
+      v-on:selectNote="selectNote"
+      :key="note.id"
+    />
   </div>
 </template>
 
@@ -12,6 +16,22 @@ import NoteSelector from "./NoteSelector.vue";
 export default {
   components: {
     NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "This is a first test", timestamp: Date.now() },
+        { id: 2, body: "This is a second test", timestamp: Date.now() },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+        { id: 4, body: "This is a fourth test", timestamp: Date.now() },
+      ],
+      selectedNoteId: 1,
+    };
+  },
+  methods: {
+    selectNote: function (note) {
+      this.selectedNoteId = note.id;
+    },
   },
 };
 </script>
