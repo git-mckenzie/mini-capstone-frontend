736145784c427ed2fcfd0df2689d9a871aacfc3e
Jason McKenzie <30478073+git-mckenzie@users.noreply.github.com>
2021-08-04 13:01:14 -0500

    Delete vue-notes directory

--- vue-notes/src/components/Toolbar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="toolbar">
-    <button class="toolbar-button">New</button>
-    <button class="toolbar-button">Delete</button>
-    <input class="toolbar-search" type="text" placeholder="Search..." />
-  </div>
-</template>
